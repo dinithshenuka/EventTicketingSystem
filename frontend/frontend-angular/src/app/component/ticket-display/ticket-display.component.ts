3efcb627d43f3f58c0efa52c4875acc88d3268d1
Dinith <dinithshenuka@gmail.com>
2024-12-03 16:54:16 +0530

    few components added

---
+++ frontend/frontend-angular/src/app/component/ticket-display/ticket-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket-display.component.html',
+  styleUrl: './ticket-display.component.css'
+})
+export class TicketDisplayComponent {
+
+}
