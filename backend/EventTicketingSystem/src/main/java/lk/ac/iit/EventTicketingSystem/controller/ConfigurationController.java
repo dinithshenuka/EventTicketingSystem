e0ec74e160d1efe16cc41e95364bc6588143312c
Dinith <dinithshenuka@gmail.com>
2024-12-11 15:07:34 +0530

    backend - release and retrive finalise

--- backend/EventTicketingSystem/src/main/java/lk/ac/iit/EventTicketingSystem/controller/ConfigurationController.java
+++ backend/EventTicketingSystem/src/main/java/lk/ac/iit/EventTicketingSystem/controller/ConfigurationController.java
@@ -4,17 +4,17 @@ import lk.ac.iit.EventTicketingSystem.SystemConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/configuration")
 public class ConfigurationController {
 
+    private final SystemConfiguration systemConfiguration;
+
     @Autowired
     public ConfigurationController(SystemConfiguration systemConfiguration) {
+        this.systemConfiguration = systemConfiguration;
     }
 
     // update the system configuration
@@ -22,6 +22,19 @@ public class ConfigurationController {
     public ResponseEntity<SystemConfiguration> updateConfiguration(@RequestBody SystemConfiguration systemConfiguration) {
         SystemConfiguration updatedConfiguration = systemConfiguration.updateConfiguration(systemConfiguration);
         return new ResponseEntity<>(updatedConfiguration, HttpStatus.OK);
+    }
+
+    // start and stop
+    @PostMapping("/start")
+    public ResponseEntity<Void> start() {
+        systemConfiguration.setRunning(true);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/stop")
+    public ResponseEntity<Void> stop() {
+        systemConfiguration.setRunning(false);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
