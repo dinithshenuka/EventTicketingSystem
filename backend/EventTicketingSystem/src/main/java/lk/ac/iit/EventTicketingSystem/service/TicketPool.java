dcb0bd00be1de4db35e6f45557aa11715f24e258
Dinith <dinithshenuka@gmail.com>
2024-12-11 15:56:02 +0530

    backend done

--- backend/EventTicketingSystem/src/main/java/lk/ac/iit/EventTicketingSystem/service/TicketPool.java
+++ backend/EventTicketingSystem/src/main/java/lk/ac/iit/EventTicketingSystem/service/TicketPool.java
@@ -1,9 +1,11 @@
 package lk.ac.iit.EventTicketingSystem.service;
 
+import lk.ac.iit.EventTicketingSystem.SystemConfiguration;
 import lk.ac.iit.EventTicketingSystem.models.Event;
 import lk.ac.iit.EventTicketingSystem.models.Ticket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,14 +18,32 @@ import java.util.stream.Collectors;
 @Service
 public class TicketPool {
     private final Map<Long, BlockingQueue<Ticket>> ticketPoolMap = new ConcurrentHashMap<>();
+    private int poolSize = 0;
+    private int totalPoolTickets = 0;
+    private final SystemConfiguration systemConfiguration;
+
+    @Autowired
+    public TicketPool(SystemConfiguration systemConfiguration) {
+        this.systemConfiguration = systemConfiguration;
+    }
 
     // logger
     Logger logger = LoggerFactory.getLogger(TicketPool.class);
 
     public void addToTicketPool(Long eventId, Ticket ticket) {
+        if (poolSize >= systemConfiguration.getMaxTicketCapacity() ) {
+            throw new IllegalStateException("Cannot add more tickets, pool size exceeds maximum capacity.");
+        } else if (totalPoolTickets >= systemConfiguration.getTotalTickets()) {
+            throw new IllegalStateException("Cannot add more tickets, total tickets exceeds maximum system capacity.");
+        }
         ticketPoolMap.computeIfAbsent(eventId, e -> new LinkedBlockingQueue<>()).add(ticket);
         logger.info("Ticket added to the pool: {}", ticket);
+        poolSize++;
+        totalPoolTickets++;
+        logger.info("Pool size: {}", poolSize);
+        logger.info("Total Number of Tickets handled: {}", totalPoolTickets);
     }
+
 
     public Ticket removeFromTicketPool(Long eventId) throws InterruptedException {
         BlockingQueue<Ticket> ticketQueue = ticketPoolMap.get(eventId);
@@ -31,6 +51,8 @@ public class TicketPool {
             throw new IllegalArgumentException("No tickets available for the specified event.");
         }
         logger.info("Ticket removed from the pool: {}", ticketQueue.peek());
+        poolSize--;
+        logger.info("Pool size: {}", poolSize);
         return ticketQueue.take();
     }
 
